0f5885cdbc3ec076806b955f6500ff52c4712778
BUG95 <butnaru.gheorghita@yahoo.com>
2022-08-31 13:25:27 +0300

    Initial Commit

---
+++ src/main/java/com/spring/fruitmvcrest/FruitMvcRestApplication.java
@@ -0,0 +1,13 @@
+package com.spring.fruitmvcrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitMvcRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitMvcRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/fruitmvcrest/FruitMvcRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.fruitmvcrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitMvcRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
